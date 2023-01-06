d63f099c2fdf281df456859bcfb67c1e8e4eda8f
iigmir <roc120j@gmail.com>
2023-01-07 00:24:50 +0800

    Test fix

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,6 @@
 import FetchPolyfill from "node-fetch";
 import FetchPostAPI from "./app/fetch-post-api.js";
-import FetchPost from "./app/fetch-post.js";
+import FetchPosts from "./app/fetch-posts.js";
 import { strictEqual } from "assert";
 
 const account = "hiten";
@@ -17,10 +17,10 @@ describe("FetchPostAPI", () => {
     });
 });
 
-describe("FetchPost", () => {
+describe("FetchPosts", () => {
     it("should get posts", async () => {
         const url = "https://api.fanbox.cc/post.listCreator?creatorId=hiten&maxPublishedDatetime=2022-12-18 22:08:31&maxId=4949682&limit=80";
-        const result = await FetchPost(url);
+        const result = await FetchPosts(url);
         strictEqual( result[0].id, "4949682" );
         strictEqual( result[64].id, "230963" );
     });
