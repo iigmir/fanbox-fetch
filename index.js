4491fa793f0c250e2d9f1e335fb4de34d1dad6b7
iigmir <roc120j@gmail.com>
2023-01-07 00:36:14 +0800

    Main scripts

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// Packages
 import FetchPolyfill from "node-fetch";
+import { mkdir, writeFile } from "node:fs/promises";
+// Scripts
+import FetchPostAPI from "./app/fetch-post-api.js";
+import FetchPosts from "./app/fetch-posts.js";
 
 if( fetch == undefined ) {
     fetch = FetchPolyfill;
 }
 
-const main = (account = "") => {};
+const main = async (account = "") => {
+    if( account.length < 1 || account == undefined ) {
+        throw new Error("No account given");
+    }
+    const result_path = `./results/${account}`;
+    await mkdir(result_path);
+    const apiurl = await FetchPostAPI(account);
+    const posts = await FetchPosts(apiurl);
+    await writeFile(`${result_path}/posts.json`, JSON.stringify(posts));
+};
 
 main(process.argv[2]);
