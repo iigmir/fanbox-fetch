26434d74c37c4eb343ec702aa2fb127ed875d26d
iigmir <roc120j@gmail.com>
2023-01-07 00:26:58 +0800

    fetch posts

--- app/fetch-posts.js
+++ app/fetch-posts.js
@@ -2,13 +2,6 @@ import fetch_options from "./options.js";
 
 export default async (url = "") => {
     const { body } = await fetch( url, fetch_options ).then( r => r.json() );
-    // const get_params = (body = [""]) => {
-    //     const search = new URL(body[0]).search.slice(1);
-    //     const params_array = search.split( "&" ).map( a => a.split("=") );
-    //     const params = Object.fromEntries( params_array );
-    //     return { ...params, limit: String( params.limit * body.length ) };
-    // }
-    // const params = new URLSearchParams( get_params(body) ).toString();
-    return body;
+    return body.items;
 };
 
