9e90809b240574a60bcc8085dbe39bf9066aca2e
iigmir <roc120j@gmail.com>
2023-01-07 10:22:47 +0800

    Do script

--- fetch-single-file.js
+++ fetch-single-file.js
@@ -1,6 +1,18 @@
+import PostScript from "./single-post.js";
+
 const account = process.argv[2];
 const id = process.argv[3];
-const result_path = `./results/${account}`;
-const post = { id };
 
-console.log(result_path, post);
+const main = async (account = "", id = "") => {
+    if( account.length < 1 || account == undefined ) {
+        throw new Error("No account given");
+    }
+    if( id.length < 1 || id == undefined ) {
+        throw new Error("No account given");
+    }
+    const result_path = `./results/${account}`;
+    const post = { id };
+    PostScript(result_path, post);
+}
+
+main(account, id);
