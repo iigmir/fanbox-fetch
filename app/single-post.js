571f9453b2eb04ed64e0cb78e44318c7bcff312b
iigmir <roc120j@gmail.com>
2023-01-14 10:37:51 +0800

    Fix the API

--- app/single-post.js
+++ app/single-post.js
@@ -11,7 +11,7 @@ export default (root_path = "./results/example") => {
         console.log("Downloading: " + post.id);
         const result = await FetchPost(post.id);
         const result_path = `${root_path}/${result.postId}`;
-        const images = result.post.body.images;
+        const images = Object.values( result.post.body.imageMap );
         // AJAX
         await create_dir(result_path);
         await writeFile(`${result_path}/metafile.json`, JSON.stringify(result.post));
