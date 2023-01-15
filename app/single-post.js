45b0b935bded8ed7b5f58cd59c69777267a37851
iigmir <roc120j@gmail.com>
2023-01-15 16:26:05 +0800

    Fix images

--- app/single-post.js
+++ app/single-post.js
@@ -3,18 +3,20 @@ import { writeFile } from "node:fs/promises";
 // Scripts
 import { FetchPost } from "../lib/single-post.js";
 import { create_dir } from "../lib/fs.js";
-import { FetchImageAction } from "../lib/image.js";
+// import { FetchImageAction } from "../lib/image.js";
 import { PostInfoInterface } from "../lib/interfaces.js";
+import { GetImages } from "../lib/data-processing/index.js";
 
 export default (root_path = "./results/example") => {
     return async (post = PostInfoInterface) => {
         console.log("Downloading: " + post.id);
         const result = await FetchPost(post.id);
         const result_path = `${root_path}/${result.postId}`;
-        const images = Object.values( result.post.body.imageMap );
+        const images = GetImages( result.post.body );
         // AJAX
         await create_dir(result_path);
-        await writeFile(`${result_path}/metafile.json`, JSON.stringify(result.post));
+        await writeFile(`${result_path}/metadata.json`, JSON.stringify(result.post));
+        await writeFile(`${result_path}/images.json`, JSON.stringify(images));
         if (Array.isArray(images)) {
             await FetchImageAction(images, root_path, result.post.id);
         }
