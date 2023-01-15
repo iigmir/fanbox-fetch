874360a3fa2bce677dfec2277ea7b860e5ccc569
iigmir <roc120j@gmail.com>
2023-01-15 17:36:36 +0800

    Refactor writeFile

--- app/single-post.js
+++ app/single-post.js
@@ -8,17 +8,21 @@ import { PostInfoInterface } from "../lib/interfaces.js";
 import { GetImages } from "../lib/data-processing/index.js";
 
 export default (root_path = "./results/example") => {
+    const write_data_infos = (result_path, contents, metadata, images) => {
+        return Promise.all([
+            writeFile(`${result_path}/contents.json`, JSON.stringify(contents)),
+            writeFile(`${result_path}/metadata.json`, JSON.stringify(metadata)),
+            writeFile(`${result_path}/images.json`, JSON.stringify(images)),
+        ]);
+    };
     return async (post = PostInfoInterface) => {
         console.log("Downloading: " + post.id);
         const result = await FetchPost(post.id);
         const result_path = `${root_path}/${result.postId}`;
-        const images_source = GetImages(result.post.body);
-        const images = images_source.map( ({ content }) => content );
+        const images = GetImages(result.post.body).map( ({ content }) => content );
         // AJAX
         await create_dir(result_path);
-        await writeFile(`${result_path}/metadata.json`, JSON.stringify(result.post));
-        await writeFile(`${result_path}/images.json`, JSON.stringify(images));
-        await writeFile(`${result_path}/contents.json`, JSON.stringify(result));
+        await write_data_infos( result_path, result, result.post, images );
         if (Array.isArray(images)) {
             await FetchImageAction(images, root_path, result.post.id);
         }
