a3d2d913e7ea5f3ca5e5c338e77145a5ec906c88
iigmir <roc120j@gmail.com>
2023-01-15 17:27:15 +0800

    Change version detection

--- app/single-post.js
+++ app/single-post.js
@@ -12,11 +12,13 @@ export default (root_path = "./results/example") => {
         console.log("Downloading: " + post.id);
         const result = await FetchPost(post.id);
         const result_path = `${root_path}/${result.postId}`;
-        const images = GetImages( result.post.body ).map( ({ content }) => content );
+        const images_source = GetImages(result.post.body);
+        const images = images_source.map( ({ content }) => content );
         // AJAX
         await create_dir(result_path);
         await writeFile(`${result_path}/metadata.json`, JSON.stringify(result.post));
         await writeFile(`${result_path}/images.json`, JSON.stringify(images));
+        await writeFile(`${result_path}/contents.json`, JSON.stringify(result));
         if (Array.isArray(images)) {
             await FetchImageAction(images, root_path, result.post.id);
         }
