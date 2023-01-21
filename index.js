c8a4305d9f7c73b6b25e709a2bb4c08ee4888774
iigmir <roc120j@gmail.com>
2023-01-22 07:59:46 +0800

    Entry write

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import { main } from "./app/posts.js";
+import { main as main_by_account } from "./app/posts.js";
+import { main as main_by_url_params } from "./app/fetch-single-file.js";
+import { GetParsedUrl } from "./app/helpers.js";
 
-const account = process.argv[2];
+const input = process.argv[2];
 
-main(account);
+// Conditions
+const is_url = /^http/.test(input);
+
+if( is_url ) {
+    const parsed_url = GetParsedUrl(input);
+    main_by_url_params( parsed_url.account, parsed_url.id );
+} else {
+    main_by_account(input);
+}
